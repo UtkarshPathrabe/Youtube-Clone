10989ea89a25720fc58d34a473ca1360cc992ea1
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-05 13:09:17 +0530

    Update Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,7 +6,7 @@ const Videos = ({ videos, direction }) => {
     return 'Loading...';
   }
   return (
-    <Stack direction={ direction ?? 'row' } flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={ direction ?? 'row' } flexWrap="wrap" justifyContent="flex-start" gap={2}>
       {videos.map((item, index) => (
         <Box key={index}>
           {(item?.id?.videoId) ? (<VideoCard videoDetail={item} />) : null}
