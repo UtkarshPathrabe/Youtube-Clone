c7d0f066bb4f4d2b844dc43365202033c91b9a09
Utkarsh Pathrabe <uapathrabe@gmail.com>
2022-10-05 12:53:59 +0530

    Update VideoCard.jsx

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -12,7 +12,7 @@ const VideoCard = ({ videoDetail }) => {
         <CardMedia
           image={snippet?.thumbnails?.high?.url ?? demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{ width: { xs: '100%', sm: '358px' }, height: 180 }}
+          sx={{ width: { xs: '100%', sm: '358px', md: '320px' }, height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
